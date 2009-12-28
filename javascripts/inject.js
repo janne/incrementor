b554b1d82fdad3b60331dd6f224d53826a4c1189
Jan Andersson <jan.andersson@gear.se>
2009-12-28 12:30:21 +0100

    Fix problem with keybaord shortcuts

--- javascripts/inject.js
+++ javascripts/inject.js
@@ -7,16 +7,14 @@ chrome.extension.sendRequest("regexp", function(response) {
     chrome.extension.sendRequest("show");
 
     // Handle keydown
-    jQuery(document).ready(function() {
-      jQuery(window).bind('keydown',function(e) {
-        if (e.ctrlKey && e.altKey) {
-          if (e.keyCode == 37) {
-            chrome.extension.sendRequest("decrement");
-          } else if (e.keyCode == 39) {
-            chrome.extension.sendRequest("increment");
-          }
+    jQuery(window).bind('keydown',function(e) {
+      if (e.ctrlKey && e.altKey) {
+        if (e.keyCode == 37) {
+          chrome.extension.sendRequest("decrement");
+        } else if (e.keyCode == 39) {
+          chrome.extension.sendRequest("increment");
         }
-      });
+      }
     });
   }
 });
